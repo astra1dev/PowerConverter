713e27cd15ed63470d64a01ed1abfd5f1890e4fb
kk~best <90265231+kkb3st@users.noreply.github.com>
2022-12-25 23:24:40 +0100

    Update console.py

--- console.py
+++ console.py
@@ -12,34 +12,80 @@ console_banner = r"""
  |_|   \___/ \_/\_/ \___|_|   \_____\___/|_| |_|\_/ \___|_|   \__\___|_|    
 """
 
-print(console_banner + "\n\nWelcome to PowerConverter!")
-INPUT_Converter = input("Available converters: png2jpg, jpg2png, png2ico, ico2png, png2webp, webp2png, mov2mp4, mp42mov,"
-                        " wav2mp3, mp32wav, yt2mp3, yt2mp4\n"
-                        "\nPlease enter the converting method you want:").lower()
+converters = """
+------------------------------------------------
+IMAGE:
+png2jpg     jpg2png     ico2png     webp2png
+png2ico     jpg2ico     ico2jpg     webp2ico
+png2webp    jpg2webp    ico2webp    webp2jpg
+------------------------------------------------
+VIDEO: (ffmpeg required)
+mp42mov     mov2mp4
+------------------------------------------------
+AUDIO: (ffmpeg required)
+mp32wav     wav2mp3
+------------------------------------------------
+YOUTUBE DOWNLOADER: (ffmpeg required)
+yt2mp3      yt2mp4
+------------------------------------------------
+"""
+
+print(console_banner + converters)
+print("\nPlease enter a converter you want to use:")
+INPUT_Converter = input(">").lower()
+
+
+# Image
 
 if INPUT_Converter == "png2jpg":
     INPUT_File = input("Please provide the absolute path of the file you want to convert: ")
     png2jpg(INPUT_File)
 
-elif INPUT_Converter == "jpg2png":
-    INPUT_File = input("Please provide the absolute path of the file you want to convert: ")
-    jpg2png(INPUT_File)
-
 elif INPUT_Converter == "png2ico":
     INPUT_File = input("Please provide the absolute path of the file you want to convert: ")
     png2ico(INPUT_File)
-
-elif INPUT_Converter == "ico2png":
-    INPUT_File = input("Please provide the absolute path of the file you want to convert: ")
-    ico2png(INPUT_File)
 
 elif INPUT_Converter == "png2webp":
     INPUT_File = input("Please provide the absolute path of the file you want to convert: ")
     png2webp(INPUT_File)
 
+elif INPUT_Converter == "jpg2png":
+    INPUT_File = input("Please provide the absolute path of the file you want to convert: ")
+    jpg2png(INPUT_File)
+
+elif INPUT_Converter == "jpg2ico":
+    INPUT_File = input("Please provide the absolute path of the file you want to convert: ")
+    jpg2ico(INPUT_File)
+
+elif INPUT_Converter == "jpg2webp":
+    INPUT_File = input("Please provide the absolute path of the file you want to convert: ")
+    jpg2webp(INPUT_File)
+
+elif INPUT_Converter == "ico2png":
+    INPUT_File = input("Please provide the absolute path of the file you want to convert: ")
+    ico2png(INPUT_File)
+
+elif INPUT_Converter == "ico2jpg":
+    INPUT_File = input("Please provide the absolute path of the file you want to convert: ")
+    ico2jpg(INPUT_File)
+
+elif INPUT_Converter == "ico2webp":
+    INPUT_File = input("Please provide the absolute path of the file you want to convert: ")
+    ico2webp(INPUT_File)
+
 elif INPUT_Converter == "webp2png":
     INPUT_File = input("Please provide the absolute path of the file you want to convert: ")
     webp2png(INPUT_File)
+
+elif INPUT_Converter == "webp2ico":
+    INPUT_File = input("Please provide the absolute path of the file you want to convert: ")
+    webp2ico(INPUT_File)
+
+elif INPUT_Converter == "webp2jpg":
+    INPUT_File = input("Please provide the absolute path of the file you want to convert: ")
+    webp2jpg(INPUT_File)
+
+# Video
 
 elif INPUT_Converter == "mov2mp4":
     INPUT_File = input("Please provide the absolute path of the file you want to convert: ")
@@ -49,6 +95,8 @@ elif INPUT_Converter == "mp42mov":
     INPUT_File = input("Please provide the absolute path of the file you want to convert: ")
     mp42mov(INPUT_File)
 
+# Audio
+
 elif INPUT_Converter == "wav2mp3":
     INPUT_File = input("Please provide the absolute path of the file you want to convert: ")
     wav2mp3(INPUT_File)
@@ -56,6 +104,8 @@ elif INPUT_Converter == "wav2mp3":
 elif INPUT_Converter == "mp32wav":
     INPUT_File = input("Please provide the absolute path of the file you want to convert: ")
     mp32wav(INPUT_File)
+
+# YouTube Downloader
 
 elif INPUT_Converter == "yt2mp3":
     INPUT_Link = input("Please provide a YouTube-Video Link you want to download: ")
