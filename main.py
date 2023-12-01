78cc3f8280138caf9012fe634058c4ecf2ae944c
Astral✨ <90265231+astra1dev@users.noreply.github.com>
2023-12-01 18:20:31 +0100

    Update to v1.6.4

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ banner_console = Fore.GREEN + r"""
 | |  | (_) \ V  V /  __/ |  | |___| (_) | | | \ V /  __/ |  | ||  __/ |    
 |_|   \___/ \_/\_/ \___|_|   \_____\___/|_| |_|\_/ \___|_|   \__\___|_|    
 
-V.1.6.2                                 created by kk, github.com/kk-dev7
+V.1.6.4                                 created by kk, github.com/kk-dev7
 """ + Style.RESET_ALL
 
 banner_converters = """
@@ -25,33 +25,21 @@ Supported File Types:
 Image:      .png     .jpg     .ico     .webp
 Video:      .mp4     .avi     .mov     .flv
 Audio:      .mp3     .wav   
-YouTube:    yt-mp3      yt-mp4      yt-thumb
 """
 
-banner_usage = Fore.BLUE + "\nInput Format: [FileType1]-[FileType2]  ---> example: flv-mp4\n" + Style.RESET_ALL
+banner_usage = Fore.BLUE + "\nInput Format: to-[DesiredOutputFormat]  ---> example: to-mp4\n" + Style.RESET_ALL
 
 # -------------------- VARIABLES -------------------- #
-user_continue = "y"
 
 converters = {
     # Image
-    "png-jpg": png2jpg, "png-ico": png2ico, "png-webp": png2webp,
-    "jpg-png": jpg2png, "jpg-ico": jpg2ico, "jpg-webp": jpg2webp,
-    "ico-png": ico2png, "ico-jpg": ico2jpg, "ico-webp": ico2webp,
-    "webp-png": webp2png, "webp-ico": webp2ico, "webp-jpg": webp2jpg,
+    "to-jpg": anything_to_jpg, "to-ico": anything_to_ico, "to-webp": anything_to_webp, "to-png": anything_to_png,
 
     # Video
-    "mp4-mov": mp42mov, "mp4-avi": mp42avi, "mp4-flv": mp42flv,
-    "mov-mp4": mov2mp4, "mov-avi": mov2avi, "mov-flv": mov2flv,
-    "avi-mp4": avi2mp4, "avi-mov": avi2mov, "avi-flv": avi2flv,
-    "flv-mp4": flv2mp4, "flv-mov": flv2mov, "flv-avi": flv2avi,
+    "to-mov": anything_to_mov, "to-mp4": anything_to_mp4, "to-avi": anything_to_avi, "to-flv": anything_to_flv,
 
     # Audio
-    "wav-mp3": wav2mp3, "mp3-wav": mp32wav,
-}
-
-yt_converters = {
-    "yt-mp3": yt2mp3, "yt-mp4": yt2mp4, "yt-thumb": ytthumb,
+    "wav-mp3": wav_to_mp3, "mp3-wav": mp3_to_wav,
 }
 
 
@@ -59,12 +47,8 @@ yt_converters = {
 def convert(c):
     if c in converters:
         f = input("\n[-] Please provide the path of the file you want to convert: ")
-        f = [f]
+        f = [f.strip() for f in f.split(';')]
         converters[c](f)
-    elif c in yt_converters:
-        f = input("\n[-] Please provide a YouTube-Video Link you want to download: ")
-        f = [f]
-        yt_converters[c](f)
     else:
         print(Fore.RED + "[ERROR] Please provide a valid converter!" + Style.RESET_ALL)
 
@@ -72,6 +56,7 @@ def convert(c):
 def advanced_convert(c, f):  # c = converter, f = files
     if not f:
         print(Fore.RED + "[ERROR] Please provide either a single file or a list of files." + Style.RESET_ALL)
+        return
 
     # if the user provided a single file, make a list out of it
     if not isinstance(f, list):
@@ -80,8 +65,6 @@ def advanced_convert(c, f):  # c = converter, f = files
     # check the given converter and if valid, convert the file
     if c in converters:
         converters[c](f)
-    elif c in yt_converters:
-        yt_converters[c](f)
     else:
         print(Fore.RED + "[ERROR] Please provide a valid converter!" + Style.RESET_ALL)
 
@@ -90,38 +73,44 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-# DEFAULT / USER-FRIENDLY MODE
-if not len(sys.argv) > 1:
-    while user_continue == "y":  # While the user wants to continue converting...
-        print(banner_console + banner_converters + banner_usage)
-        print("[-] Please enter a converter you want to use:")
-        convert(input(">").lower())
-        print("\n[-] Do you want to continue converting? (y/n)")
-        user_continue = input(">")
-        clear_screen()
+def main():
+    user_continue = "y"
+    # DEFAULT / USER-FRIENDLY MODE
+    if not len(sys.argv) > 1:
+        while user_continue.lower() == "y":  # While the user wants to continue converting...
+            print(banner_console + banner_converters + banner_usage)
+            print("[-] Please enter a converter you want to use:")
+            convert(input(">"))
+            print("\n[-] Do you want to continue converting? (y/n)")
+            user_continue = input(">")
+            clear_screen()
 
-
-# ADVANCED MODE
-else:
-    print(banner_console)
-    # argumentList = sys.argv[1:]
-
-    parser = argparse.ArgumentParser(description=None)
-
-    parser.add_argument("-f", "--file", help="Choose a file to convert")
-    parser.add_argument("-c", "--converter", help="Choose a conversion method")
-    parser.add_argument("-l", "--list", help="Choose a list of files to convert (semicolon-separated)")
-
-    args = parser.parse_args()
-
-    if args.file and args.list:
-        print(Fore.RED + "[ERROR] Please provide either a single file or a list of files, not both." + Style.RESET_ALL)
-
-    if args.file:
-        # if a single file is given, pass it to convert function
-        advanced_convert(args.converter, args.file)
-    elif args.list:
-        # if the argument file list is given, pass the list to convert function
-        advanced_convert(args.converter, [f.strip() for f in args.list.split(';')])
+    # ADVANCED MODE
     else:
-        print(Fore.RED + "[ERROR] Please specify a file or a list of files to convert!")
+        print(banner_console)
+        # argumentList = sys.argv[1:]
+
+        parser = argparse.ArgumentParser(description=None)
+
+        parser.add_argument("-f", "--file", help="Choose a file to convert")
+        parser.add_argument("-c", "--converter", help="Choose a conversion method")
+        parser.add_argument("-l", "--list", help="Choose a list of files to convert (semicolon-separated)")
+
+        args = parser.parse_args()
+
+        if args.file and args.list:
+            print(Fore.RED + "[ERROR] Please provide either a single file or a list of files, not both." +
+                  Style.RESET_ALL)
+
+        if args.file:
+            # if a single file is given, pass it to convert function
+            advanced_convert(args.converter, args.file)
+        elif args.list:
+            # if the argument file list is given, pass the list to convert function
+            advanced_convert(args.converter, [f.strip() for f in args.list.split(';')])
+        else:
+            print(Fore.RED + "[ERROR] Please specify a file or a list of files to convert!")
+
+
+if __name__ == "__main__":
+    main()
